86114a1118a21c7948df3dba7c9ac95d12c6e4f3
Roma-x <56560165+Roma-x@users.noreply.github.com>
2024-05-09 00:22:28 +0300

    backend start

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your Vue.js client's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // Allow cookies to be sent with requests
+}));
+
+mongoose.connect('mongodb+srv://tsymbaljuk2001:messi10ronaldo7@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const productRoutes = require('./routes/productRoutes'); // Adjust the path as needed
+
+app.use(bodyParser.json());
+app.use(productRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
