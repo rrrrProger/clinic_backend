5842022056d1fc0018a70665845266d838288e83
roman <tsymbaljuk2001@gmail.com>
2024-05-09 14:45:47 +0300

    Added serve and dist

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const cors = require('cors');
 
 const app = express();
 const port = 3001;
-app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your Vue.js client's URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-    credentials: true, // Allow cookies to be sent with requests
-}));
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 mongoose.connect('mongodb+srv://tsymbaljuk2001:messi10ronaldo7@cluster0.k870rvy.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
