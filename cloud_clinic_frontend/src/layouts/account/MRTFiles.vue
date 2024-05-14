d5c3a7d84e7ebe6123e9c5acc4d0719aba6621b9
roman <tsymbaljuk2001@gmail.com>
2024-05-14 12:02:08 +0300

    Returned back

--- cloud_clinic_frontend/src/layouts/account/MRTFiles.vue
+++ cloud_clinic_frontend/src/layouts/account/MRTFiles.vue
@@ -1,18 +1,6 @@
 <template>
-    <input type="file" @change="getViewFiles($event)">
-    <div id="dwv-simpl0" class="dwv"></div>
-    <!-- Third party (dwv) -->
-    <script type="text/javascript" src="../../../../node_modules/jszip/dist/jszip.min.js"></script>
-    <script type="text/javascript" src="../../../../node_modules/konva/konva.min.js"></script>
-    <!-- decoders -->
-    <script type="text/javascript" src="../../../../node_modules/dwv/decoders/dwv/rle.js"></script>
-    <script type="text/javascript" src="../../../../node_modules/dwv/decoders/pdfjs/jpx.js"></script>
-    <script type="text/javascript" src="../../../../node_modules/dwv/decoders/pdfjs/util.js"></script>
-    <script type="text/javascript" src="../../../../node_modules/dwv/decoders/pdfjs/arithmetic_decoder.js"></script>
-    <script type="text/javascript" src="../../../../node_modules/dwv/decoders/pdfjs/jpg.js"></script>
-    <script type="text/javascript" src="../../../../node_modules/dwv/decoders/rii-mango/lossless-min.js"></script>
-    <!-- dwv -->
-    <script type="text/javascript" src="../../../../node_modules/dwv/dist/dwv.min.js"></script>
+    <div id="dwvAppp">
+    </div>
 </template>
 <script setup lang="ts">
 import {
@@ -24,7 +12,14 @@ import {
   getDwvVersion,
 } from 'dwv';
 
-var dwv = dwv || {};
+// Image decoders (for web workers)
+decoderScripts.jpeg2000 = '../../../../node_modules/dwv/decoders/pdfjs/decode-jpeg2000.js'
+decoderScripts['jpeg-lossless'] =
+  '../../../../node_modules/dwv/decoders/rii-mango/decode-jpegloss.js'
+decoderScripts['jpeg-baseline'] =
+  '../../../../node_modules/dwv/decoders/pdfjs/decode-jpegbaseline.js'
+decoderScripts.rle = '../../../../node_modules/dwv/decoders/dwv/decode-rle.js'
+
 
 var dwvApp = new App();
 const viewConfig0 = new ViewConfig('dwvAppp');
@@ -36,53 +31,11 @@ var tools = {
     WindowLevel: new ToolConfig(),
     Draw: new ToolConfig(['Ruler']),
 };
-
+var inputUrls = [ 'https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323851.dcm' ];
 options.tools = tools;
-
-// "x-lml/x-evm"
-
 dwvApp.init(options);
 console.log('HERE');
-
-dwvApp.addEventListener('renderend', function (/*event*/) {
-    console.log('render endend');
-});
-
-dwvApp.addEventListener('loaditem', function (/*event*/) {
-    console.log('loaditem');
-});
-
-dwvApp.addEventListener('loadstart', function (/*event*/) {
-    console.log('loadstart');
-});
-
-dwvApp.addEventListener('loaderror', function (event) {
-    console.log('loaderror');
-    console.log(event);
-});
-
-dwvApp.addEventListener('loadend', function (/*event*/) {
-    console.log('loadend');
-});
-
-
-//dwvApp.image.decoderScripts = {
-//        "jpeg2000": "decoders/pdfjs/decode-jpeg2000.js",
-//        "jpeg-lossless": "decoders/rii-mango/decode-jpegloss.js",
-//        "jpeg-baseline": "decoders/pdfjs/decode-jpegbaseline.js"
-//    };
-function getViewFiles(e) {
-    const target = e.target as HTMLInputElement;
-
-    console.log('Go to getFileToView');
-    if (target && target.files) {
-        const files = Array.from(target.files);
-        for (var file of target.files) {
-            console.log("file : ", file);
-        }
-
-        console.log('files: ', files);
-        dwvApp.loadFiles(files);
-    }
+if( inputUrls && inputUrls.length > 0 ) {
+    dwvApp.loadURLs(inputUrls);
 }
 </script>
